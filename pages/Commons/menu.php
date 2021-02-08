b0aa7c28bec8a55b8c74f4ccdb032e8f7c2fb7d0
serge <serge.moi@laplateforme.io>
2021-02-08 14:54:03 +0100

    Début du projet refonte site asso animal h2prog Mise en place de la structure du site Header - menu

---
+++ pages/Commons/menu.php
@@ -0,0 +1,56 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle perso_ColorRoseMenu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                    L'asso
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item perso_ColorRoseMenu" href="#">Action</a></li>
+                    <li><a class="dropdown-item perso_ColorRoseMenu" href="#">Another action</a></li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle perso_ColorOrangeMenu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                    Pensionnaires
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item perso_ColorOrangeMenu" href="#">Action</a></li>
+                    <li><a class="dropdown-item perso_ColorOrangeMenu" href="#">Another action</a></li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle perso_ColorRougeMenu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                    Actus
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item perso_ColorRougeMenu" href="#">Action</a></li>
+                    <li><a class="dropdown-item perso_ColorRougeMenu" href="#">Another action</a></li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle perso_ColorVertMenu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                    Conseils
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item perso_ColorVertMenu" href="#">Action</a></li>
+                    <li><a class="dropdown-item perso_ColorVertMenu" href="#">Another action</a></li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle perso_ColorBleuCielMenu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                    Contacts
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item perso_ColorBleuCielMenu" href="#">Action</a></li>
+                    <li><a class="dropdown-item perso_ColorBleuCielMenu" href="#">Another action</a></li>
+                </ul>
+            </li>
+
+        </ul>
+    </div>
+</nav>
